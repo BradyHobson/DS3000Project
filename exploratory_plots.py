65b45a76c6729ad2ec2ef76b73b227405398a41f
Jane <ytzheng2003@gmail.com>
2022-12-01 15:52:21 -0500

    Updated

--- exploratory_plots.py
+++ exploratory_plots.py
@@ -8,9 +8,7 @@ def scatter_plot(df, col1, col2):
     legend_labels, _ = ax.get_legend_handles_labels()
     ax.legend(legend_labels, ["Yes", 'No'], title="Attrition")
     ax.set_title(str(col1) + " v.s " + str(col2))
-
-    plt.savefig("Scatter plot " + col1 + " v.s " + col2 + ".png")
-
+    plt.show()
     return plt
 
 
@@ -25,22 +23,22 @@ def heat_map(df):
 def main():
     df = pd.read_csv("Attrition_dummy.csv")
     heat_map(df)
-    # a = scatter_plot(df, "Age", "JobLevel")
-    # b = scatter_plot(df, "Age", "TotalWorkingYears")
-    # c = scatter_plot(df, "JobLevel", "MonthlyIncome")
-    # d = scatter_plot(df, "JobLevel", "TotalWorkingYears")
-    # e = scatter_plot(df, "JobLevel", "YearsAtCompany")
-    # f = scatter_plot(df, "MaritalStatus", "StockOptionLevel")
-    # g = scatter_plot(df, "MonthlyIncome", "TotalWorkingYears")
-    # h = scatter_plot(df, "MonthlyIncome", "YearsAtCompany")
-    # i = scatter_plot(df, "PercentSalaryHike", "PerformanceRating")
-    # j = scatter_plot(df, "TotalWorkingYears", "YearsAtCompany")
-    # k = scatter_plot(df, "YearsAtCompany", "YearsInCurrentRole")
-    # l = scatter_plot(df, "YearsAtCompany", "YearsSinceLastPromotion")
-    # m = scatter_plot(df, "YearsAtCompany", "YearsWithCurrManager")
-    # n = scatter_plot(df, "YearsInCurrentRole", "YearsSinceLastPromotion")
-    # o = scatter_plot(df, "YearsInCurrentRole", "YearsWithCurrManager")
-    # p = scatter_plot(df, "YearsSinceLastPromotion", "YearsWithCurrManager")
+    scatter_plot(df, "Age", "JobLevel")
+    scatter_plot(df, "Age", "TotalWorkingYears")
+    scatter_plot(df, "JobLevel", "MonthlyIncome")
+    scatter_plot(df, "JobLevel", "TotalWorkingYears")
+    scatter_plot(df, "JobLevel", "YearsAtCompany")
+    scatter_plot(df, "MaritalStatus", "StockOptionLevel")
+    scatter_plot(df, "MonthlyIncome", "TotalWorkingYears")
+    scatter_plot(df, "MonthlyIncome", "YearsAtCompany")
+    scatter_plot(df, "PercentSalaryHike", "PerformanceRating")
+    scatter_plot(df, "TotalWorkingYears", "YearsAtCompany")
+    scatter_plot(df, "YearsAtCompany", "YearsInCurrentRole")
+    scatter_plot(df, "YearsAtCompany", "YearsSinceLastPromotion")
+    scatter_plot(df, "YearsAtCompany", "YearsWithCurrManager")
+    scatter_plot(df, "YearsInCurrentRole", "YearsSinceLastPromotion")
+    scatter_plot(df, "YearsInCurrentRole", "YearsWithCurrManager")
+    scatter_plot(df, "YearsSinceLastPromotion", "YearsWithCurrManager")
 
 
 if __name__ == "__main__":
--- project_file.py
+++ project_file.py
@@ -3,25 +3,62 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.neighbors import KNeighborsClassifier
 
 
 def linear_regress(df, meter1, meter2):
+    """ Performs Linear Regression Algorithm on data set """
+    # Split x and y into numpy arrays
     X = df[meter1].to_numpy()
     X = X.reshape(-1, 1)
     y = df[meter2].to_numpy()
     y = y.reshape(-1, 1)
+
+    # Split x and y into train and test sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=42)
     lr_model = LinearRegression(fit_intercept=True)
     lr_model = lr_model.fit(X_train, y_train)
 
+    # Find r squared value
     r_sqrd = lr_model.score(X, y)
-    print(r_sqrd)
+    print("R Squared Value: ", r_sqrd)
 
+    # Predict y values based on model
     y_pred = lr_model.predict(X_test)
 
+    # Plot a scatter plot
     plt.scatter(X_test, y_test)
     plt.plot(X_test, y_pred, color="blue", linewidth=3)
     plt.show()
+
+
+def find_best_k(X_train, X_test, y_train, y_test):
+    # build the k-nn model, experiment with different values of k and plot the results
+    accuracy = []
+    for i in range(1, 20):
+        knn = KNeighborsClassifier(n_neighbors=i).fit(X_train, y_train)
+        prediction = knn.predict(X_test)
+        accuracy.append(accuracy_score(y_test, prediction))
+
+    return accuracy.index(max(accuracy))
+
+
+def knn(df, meter1, meter2):
+    X = df[meter1]
+    y = df[meter2]
+
+    # Create test and train set
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.3)
+
+    # Find best K value to use
+    best_k = find_best_k(X_train, X_test, y_train, y_test)
+
+    knn = KNeighborsClassifier(n_neighbors=best_k)
+    knn.fit(X_train, y_train)
+    prediction = knn.predict(X_test)
+    report = classification_report(y_test, prediction)
+    return report, prediction
 
 
 if __name__ == "__main__":
@@ -30,3 +67,9 @@ if __name__ == "__main__":
     linear_regress(df, "JobLevel", "MonthlyIncome")
     linear_regress(df, "TotalWorkingYears", "MonthlyIncome")
     linear_regress(df, "Gender", "MonthlyIncome")
+
+    report, prediction = knn(df, ["Age", "Gender", "JobLevel", "MonthlyIncome", "YearsSinceLastPromotion",
+                                  "StockOptionLevel"], "Attrition")
+    print("KNN predictions: ", prediction)
+    print("KNN Report:", report)
+    
